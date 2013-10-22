e71b8b8314e66eacb3b69c7015a7d6a0dc6f93fc
thinkpad x230i <535521469@qq.com>
2013-10-22 18:31:58 +0800

    [2013-10-22 v0.0.6 ongoing] 1、51auto改版 2、增加看车地址

--- src/main/java/pp/corleone/auto51/service/detail/Auto51DetailExtracterGroovyImp.java
+++ src/main/java/pp/corleone/auto51/service/detail/Auto51DetailExtracterGroovyImp.java
@@ -2,18 +2,16 @@ package pp.corleone.auto51.service.detail;
 
 import java.util.Date;
 
+import org.apache.commons.lang.StringUtils;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.stereotype.Component;
 
 import pp.corleone.Log;
 import pp.corleone.auto51.domain.Auto51CarInfo;
 import pp.corleone.auto51.domain.Auto51CarInfo.Auto51StatusCode;
 import pp.corleone.auto51.domain.Auto51SellerInfo;
-import pp.corleone.service.ResponseWrapper;
 
-@Component("auto51DetailExtracter")
 class Auto51DetailExtracterGroovyImp implements Auto51DetailExtracter {
 	protected static void LogException(Document doc, String msg) {
 		Log.error(msg + ".." + doc.baseUri());
@@ -21,29 +19,51 @@ class Auto51DetailExtracterGroovyImp implements Auto51DetailExtracter {
 
 	protected void fillPrice(Document doc, Auto51CarInfo auto51CarInfo) {
 		try {
-			String price = doc.select("span.car_color_in").first().select("em")
-					.first().text();
+			String price = doc.select("dl.or_dl").select("b.red").first()
+					.text();
 			price = price + "万";
 			auto51CarInfo.setPrice(price);
 		} catch (Exception e) {
-			 LogException(doc, "price");
+			LogException(doc, "price");
 		}
 	}
 
 	protected void fillLicenseDateAndRoadHaul(Document doc,
 			Auto51CarInfo auto51CarInfo) {
 		try {
-			Elements infoElements = doc.select("p.car_info1");
-			if (null != infoElements) {
+			Elements infoElements = doc.select("dl.or_dl");
+
+			if (null == infoElements) {
+				LogException(doc, "license date or road haul");
+			} else {
+				String licenseDate = "";
+				String roadHaul = "";
 				Element infoElement = infoElements.first();
-				Elements strongElements = infoElement.select("strong");
-				if (strongElements.size() == 2) {
-					String licenseDate = strongElements.first().text();
-					String roadHaul = strongElements.get(1).text();
-					auto51CarInfo.setLicenseDate(licenseDate);
+				for (Element pEle : infoElement.select("p.odd_left")) {
+					if ("首次上牌：".equals(pEle.select("i").first().text())) {
+						licenseDate = pEle.text().replace("首次上牌：", "");
+					}
+				}
+				for (Element pEle : infoElement.select("p.odd_right")) {
+					if ("行驶里程：".equals(pEle.select("i").first().text())) {
+						roadHaul = pEle.text().replace("行驶里程：", "");
+					}
+				}
+
+				if (StringUtils.isBlank(roadHaul)) {
+					LogException(doc, "road haul");
+				} else {
 					auto51CarInfo.setRoadHaul(roadHaul);
 				}
+
+				if (StringUtils.isBlank(licenseDate)) {
+					LogException(doc, "licenseDate");
+				} else {
+					auto51CarInfo.setLicenseDate(licenseDate);
+				}
+
 			}
+
 		} catch (Exception e) {
 			LogException(doc, "license date or road haul");
 		}
@@ -52,64 +72,118 @@ class Auto51DetailExtracterGroovyImp implements Auto51DetailExtracter {
 	protected void fillDisplacementAndGearbox(Document doc,
 			Auto51CarInfo auto51CarInfo) {
 		try {
-			Elements infoElements = doc.select("p.car_info1");
-			if (infoElements.size() > 1) {
-				Element infoElement = infoElements.get(1);
-				Elements strongElements = infoElement.select("strong");
-				if (strongElements.size() == 2) {
-					String displacement = strongElements.first().text();
-					String gearbox = strongElements.get(1).text();
+			Elements infoElements = doc.select("dl.or_dl");
+			if (null == infoElements) {
+				LogException(doc, "displacement or gear box");
+			} else {
+				String displacement = "";
+				String gearbox = "";
+				Element infoElement = infoElements.first();
+				for (Element pEle : infoElement.select("p.odd_left")) {
+					if ("排      量：".equals(pEle.select("i").first().text())) {
+						displacement = pEle.text().replace("排      量：", "");
+						break;
+					}
+				}
+				if (StringUtils.isBlank(displacement)) {
+					LogException(doc, "displacement");
+				} else {
 					auto51CarInfo.setDisplacement(displacement);
+				}
+
+				for (Element pEle : infoElement.select("p.odd_right")) {
+					if ("变 速 箱 ：".equals(pEle.select("i").first().text())) {
+						gearbox = pEle.text().replace("变 速 箱 ：", "");
+						break;
+					}
+				}
+				if (StringUtils.isBlank(gearbox)) {
+					LogException(doc, "gearbox");
+				} else {
 					auto51CarInfo.setGearbox(gearbox);
 				}
 			}
+
 		} catch (Exception e) {
 			LogException(doc, "displacement or gear box");
 		}
 	}
 
 	protected void fillColor(Document doc, Auto51CarInfo auto51CarInfo) {
+
 		try {
-			Elements infoElements = doc.select("p.car_info1");
-			if (infoElements.size() > 2) {
-				Element infoElement = infoElements.get(2);
-				Elements strongElements = infoElement.select("strong");
-				if (strongElements.size() > 1) {
-					String color = strongElements.first().text();
+			Elements infoElements = doc.select("dl.or_dl");
+			if (null == infoElements) {
+				LogException(doc, "color");
+			} else {
+				String color = "";
+				Element infoElement = infoElements.first();
+				for (Element pEle : infoElement.select("p.odd_left")) {
+					if ("车身颜色：".equals(pEle.select("i").first().text())) {
+						color = pEle.text().replace("车身颜色：", "");
+						break;
+					}
+				}
+				if (StringUtils.isBlank(color)) {
+					LogException(doc, "color");
+				} else {
 					auto51CarInfo.setColor(color);
 				}
+
 			}
+
 		} catch (Exception e) {
 			LogException(doc, "color");
 		}
 	}
 
-	protected void fillManufacturerAndBrand(Document doc,
-			Auto51CarInfo auto51CarInfo) {
+	protected void fillManufacturer(Document doc, Auto51CarInfo auto51CarInfo) {
 		try {
-			Element paramElement = doc.select("dd.car_dsc_1>dl.params").first();
+			Element paramElement = doc.select("div.ptb_right").first();
 
-			String manufacturer = paramElement.select("dd").first().select("b")
-					.first().text();
-			auto51CarInfo.setManufacturer(manufacturer);
+			Elements manufacturers = paramElement.select("td");
 
-			String brand = doc
-					.select("div.daohang>a[href^=http://www.51auto.com/search/s-]")
-					.first().text();
+			String manufacturerString = "";
+			for (Element manufacturer : manufacturers) {
+				if ("品牌".equals(manufacturer.text())) {
+
+					manufacturerString = manufacturer.nextElementSibling()
+							.text();
+					break;
+				}
+			}
+			auto51CarInfo.setManufacturer(manufacturerString);
+			if (StringUtils.isBlank(auto51CarInfo.getManufacturer())) {
+				LogException(doc, "manufacturer");
+			}
+
+		} catch (Exception e) {
+			LogException(doc, "manufacturer");
+		}
+	}
+
+	protected void fillBrand(Document doc, Auto51CarInfo auto51CarInfo) {
+		try {
+			String title = doc.title();
+			String brand = title.substring(title.indexOf("二手"),
+					title.indexOf("】"));
 			brand = brand.replace("二手", "");
 			auto51CarInfo.setBrand(brand);
 		} catch (Exception e) {
-			LogException(doc, "manufacturer or brand");
+			LogException(doc, "brand");
 		}
 	}
 
 	protected void fillTitle(Document doc, Auto51CarInfo auto51CarInfo) {
 		try {
-			Element titleElement = doc.select("strong.car_title").first();
+			Element titleElement = doc.select("div.ocar_imp>h1").first();
 			String title = "";
 			if (titleElement != null) {
 				title = titleElement.text();
-				auto51CarInfo.setTitle(title);
+			}
+			auto51CarInfo.setTitle(title);
+			if (StringUtils.isBlank(auto51CarInfo.getTitle())) {
+				LogException(doc, "title");
 			}
 		} catch (Exception e) {
 			LogException(doc, "title");
@@ -118,7 +192,7 @@ class Auto51DetailExtracterGroovyImp implements Auto51DetailExtracter {
 
 	protected void fillContacterPhone(Document doc, Auto51CarInfo auto51CarInfo) {
 		try {
-			Element contacterPhoneElement = doc.select("div.car_ask>p").first();
+			Element contacterPhoneElement = doc.select("div.div_font").first();
 			String contacterPhone = "";
 			if (contacterPhoneElement != null) {
 				contacterPhone = contacterPhoneElement.text();
@@ -136,15 +210,34 @@ class Auto51DetailExtracterGroovyImp implements Auto51DetailExtracter {
 		}
 	}
 
+	protected void buildLookCarAddress(Document doc, Auto51CarInfo auto51CarInfo) {
+		try {
+			Element lookCarElement = doc.select("p.lookcar>i").first();
+			Element labelElement = doc.select("p.lookcar>span.gray3").first();
+
+			String value = lookCarElement.text();
+			;
+			if ("看车地点：".equals(labelElement.text())) {
+				auto51CarInfo.setParkAddress(value.replace(" 【查看地图】", ""));
+			}
+
+		} catch (Exception e) {
+			LogException(doc, "LookCarAddress or contacter");
+		}
+	}
+
 	protected Auto51CarInfo buildAuto51CarInfo(Document doc) {
 		Auto51CarInfo auto51CarInfo = new Auto51CarInfo();
 		this.fillPrice(doc, auto51CarInfo);
 		this.fillLicenseDateAndRoadHaul(doc, auto51CarInfo);
 		this.fillColor(doc, auto51CarInfo);
 		this.fillDisplacementAndGearbox(doc, auto51CarInfo);
-		this.fillManufacturerAndBrand(doc, auto51CarInfo);
+		this.fillBrand(doc, auto51CarInfo);
+		this.fillManufacturer(doc, auto51CarInfo);
 		this.fillTitle(doc, auto51CarInfo);
 		this.fillContacterPhone(doc, auto51CarInfo);
+
+		this.buildLookCarAddress(doc, auto51CarInfo);
 
 		if (this.isOnline(doc)) {
 			Date now = new Date();
@@ -165,35 +258,33 @@ class Auto51DetailExtracterGroovyImp implements Auto51DetailExtracter {
 
 	protected Auto51SellerInfo buildAuto51SellerInfo(Document doc) {
 		Auto51SellerInfo auto51SellerInfo = new Auto51SellerInfo();
-		Element pElement = doc.select("p.car_shop_info").first();
+		Element pElement = doc.select("dl.sm_dl").first();
+		String shopName = "";
 		if (pElement != null) {
-			String cityNameLiteral = "公司名称：";
-			String shopLevelLiteral = "商铺级别：";
-			// String shopAddressLiteral = "商铺地址：";
-			// Elements spanElements = pElement.select("span");
-			String info = pElement.text();
-			String shopName = info.substring(info.indexOf(cityNameLiteral)
-					+ cityNameLiteral.length(), info.indexOf(shopLevelLiteral));
-			auto51SellerInfo.setShopName(shopName.trim());
-		}
-		Element btn_shop = doc.select("a.btn_shop").first();
-		if (btn_shop != null) {
-			String shopUrl = btn_shop.attr("href");
-			// auto51SellerInfo.setShopUrl(shopUrl);
-			String zhuanqv = "http://www.51auto.com/hclist__HAPU__";
-			if (shopUrl.startsWith(zhuanqv)) {
-				String carID = shopUrl.substring(zhuanqv.length(),
-						shopUrl.indexOf("_UI__"));
-				auto51SellerInfo
-						.setShopUrl("http://www.51auto.com/control/HappyDealersList?p=p&zoneID=-1&zoneID=-1&happyUserId="
-								+ carID
-								+ "&sort=-1&level=-1&page=1&keyWord="
-								+ auto51SellerInfo.getShopName());
-			} else {
-				auto51SellerInfo.setShopUrl(shopUrl);
+			Element shopElement = pElement.select("a.a_ktit").first();
+			shopName = shopElement.text();
+			auto51SellerInfo.setShopName(shopName);
+			auto51SellerInfo.setShopUrl(shopElement.attr("href"));
+		} else {
+			Element btn_shop = doc.select("a.btn_shop").first();
+			if (btn_shop != null) {
+				String shopUrl = btn_shop.attr("href");
+				// auto51SellerInfo.setShopUrl(shopUrl);
+				String zhuanqv = "http://www.51auto.com/hclist__HAPU__";
+				if (shopUrl.startsWith(zhuanqv)) {
+					String carID = shopUrl.substring(zhuanqv.length(),
+							shopUrl.indexOf("_UI__"));
+					auto51SellerInfo
+							.setShopUrl("http://www.51auto.com/control/HappyDealersList?p=p&zoneID=-1&zoneID=-1&happyUserId="
+									+ carID
+									+ "&sort=-1&level=-1&page=1&keyWord="
+									+ auto51SellerInfo.getShopName());
+				} else {
+					auto51SellerInfo.setShopUrl(shopUrl);
+				}
 			}
-
 		}
+
 		return auto51SellerInfo;
 	}
 
--- src/test/java/pp/corleone/auto51/service/detail/Auto51DetailExtracterGroovyImpTest.java
+++ src/test/java/pp/corleone/auto51/service/detail/Auto51DetailExtracterGroovyImpTest.java
@@ -19,25 +19,29 @@ public class Auto51DetailExtracterGroovyImpTest {
 	private static Auto51DetailExtracter auto51DetailExtracter = null;
 	private static Auto51CarInfo auto51CarInfo = null;
 
-
 	@BeforeClass
 	public static void beforeClass() {
 		try {
-			doc = Jsoup.connect("http://www.51auto.com/buycar/1683912.html")
+			doc = Jsoup.connect("http://www.51auto.com/buycar/1881679.html")
 					.get();
 		} catch (IOException e) {
 			fail("set up , connection fail");
-			Log.error("",e);
+			Log.error("", e);
 		}
-		auto51DetailExtracter = new Auto51DetailExtracterGroovyImp2();
+		auto51DetailExtracter = new Auto51DetailExtracterGroovyImp();
 		auto51CarInfo = auto51DetailExtracter.getCarInfo(doc);
 	}
 
 	@Test
-	public void testbuildAuto51SellerInfo() {
-		assertEquals("北京千里行", auto51CarInfo.getAuto51SellerInfo().getShopName());
-		String shopUrl = "http://www.51auto.com/control/HappyDealersList?p=p&zoneID=-1&zoneID=-1&happyUserId=157184&sort=-1&level=-1&page=1&keyWord=北京千里行";
+	public void testbuildLookCarAddress() {
+		assertEquals("CBD店/望京店/鸟巢店/石景山店", auto51CarInfo.getAuto51SellerInfo().getShopName());
+	}
 
+	@Test
+	public void testbuildAuto51SellerInfo() {
+		assertEquals("北京卓杰行二手车", auto51CarInfo.getAuto51SellerInfo().getShopName());
+		String shopUrl = "http://www.51auto.com/dealers/75339.html";
+		
 		System.out.println(shopUrl);
 		System.out.println(auto51CarInfo.getAuto51SellerInfo().getShopUrl());
 		assertEquals(shopUrl, auto51CarInfo.getAuto51SellerInfo().getShopUrl());
@@ -45,39 +49,39 @@ public class Auto51DetailExtracterGroovyImpTest {
 
 	@Test
 	public void testfillPrice() {
-		assertEquals("4.00万", auto51CarInfo.getPrice());
+		assertEquals("3.50万", auto51CarInfo.getPrice());
 	}
 
 	@Test
 	public void testfillLicenseDateAndRoadHaul() {
-		assertEquals("2009年11月1日", auto51CarInfo.getLicenseDate());
-		assertEquals("6.27万公里", auto51CarInfo.getRoadHaul());
+		assertEquals("2010年6月(3年)", auto51CarInfo.getLicenseDate());
+		assertEquals("4.30万公里", auto51CarInfo.getRoadHaul());
 	}
 
 	@Test
 	public void testfillDisplacementAndGearbox() {
-		assertEquals("1.5L", auto51CarInfo.getDisplacement());
+		assertEquals("1.4L", auto51CarInfo.getDisplacement());
 		assertEquals("手动", auto51CarInfo.getGearbox());
 	}
 
 	@Test
 	public void testfillColor() {
-		assertEquals("红色", auto51CarInfo.getColor());
+		assertEquals("灰色/深内饰", auto51CarInfo.getColor());
 	}
 
 	@Test
 	public void testfillManufacturerAndBrand() {
-		assertEquals("长安", auto51CarInfo.getManufacturer());
-		assertEquals("悦翔", auto51CarInfo.getBrand());
+		assertEquals("北斗星", auto51CarInfo.getBrand());
+		assertEquals("昌河铃木", auto51CarInfo.getManufacturer());
 	}
 
 	@Test
 	public void testfillTitle() {
-		assertEquals("【北京】 长安 悦翔 三厢 尊贵型 手动 1.5L", auto51CarInfo.getTitle());
+		assertEquals("【北京】 昌河铃木 北斗星 ES 手动 1.4L (国IV+OBD)", auto51CarInfo.getTitle());
 	}
 
 	@Test
 	public void testfillContacterPhone() {
-		assertEquals("010-80794585", auto51CarInfo.getContacterPhone());
+		assertEquals("18001325177 4006610608", auto51CarInfo.getContacterPhone());
 	}
 }
